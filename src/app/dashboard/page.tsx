3a873b3a7c9d05d268de873805d58f18f101d219
ritik2177 <ritikkatsa2005@gmail.com>
2025-08-15 10:31:51 +0530

    update authentication

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import Navbar from '@/components/navbar'
+"use client";
 
-const page = () => {
+import { signOut, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
+export default function DashboardPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/");
+    }
+  }, [status, router]);
+
+  if (status === "loading") return <p>Loading...</p>;
+  if (!session) return null; 
 
   return (
     <div>
-      pageeeeeeeeeee
+      <h1>Welcome to Dashboard, {session.user?.name}!</h1>
+      <button onClick={() => signOut({ callbackUrl: "/" })}>
+        Logout
+      </button>
     </div>
-  )
+  );
 }
-
-export default page

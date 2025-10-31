25e2a93d86416287d83949c4a5ff6b6ed01cdcac
ritik2177 <ritikkatsa2005@gmail.com>
2025-11-01 00:42:48 +0530

    update

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
+  console.warn(" MONGODB_URI not defined — skipping DB connection (likely build phase).");
   throw new Error('Please define the MONGODB_URI environment variable');
 }
 
